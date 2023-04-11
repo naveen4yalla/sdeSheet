3caeac76a68f555165bcf55181af2020a7889137
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-04-10 20:26:19 -0700

    Time: 28 ms (89.29%), Space: 13.9 MB (52.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,22 @@
 from functools import lru_cache
+# class Solution:
+    # def rob(self, nums: List[int]) -> int:
+    #     @lru_cache(None)
+    #     def rob_helper(i):
+    #         if i>=len(nums):
+    #             return 0
+    #         ans = max(rob_helper(i+1),rob_helper(i+2)+nums[i])
+    #         return ans
+    #     return rob_helper(0)
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @lru_cache(None)
-        def rob_helper(i):
-            if i>=len(nums):
-                return 0
-            ans = max(rob_helper(i+1),rob_helper(i+2)+nums[i])
-            return ans
-        return rob_helper(0)
+        arr = []
+        if nums == []:
+            return 0
+        arr.append(0)
+        arr.append(nums[0])
+        for i in range(2,len(nums)+1):
+            arr.append( max(arr[i-1],arr[i-2]+nums[i-1]))
+        return arr[-1]
     
         
