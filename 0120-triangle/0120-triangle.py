ea4521fe72d2c8e01f7acf7b83246710bf230db2
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-04-24 19:27:59 -0700

    Time: 59 ms (95.13%), Space: 14.9 MB (53.62%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,24 +1,31 @@
 from functools import lru_cache
-class Solution:
-    def minimumTotal(self, triangle):
-        rows = len(triangle)
-        result = 0 
-        @lru_cache(None)
-        def rec(i,j):
-            if i==rows:
-                return 0
-            sum = 1000
-            f = j+1
-            for k in range(j,f+1):
-                sum = min(rec(i+1,k)+triangle[i][j],sum) 
+# class Solution:
+#     def minimumTotal(self, triangle):
+#         rows = len(triangle)
+#         result = 0 
+#         @lru_cache(None)
+#         def rec(i,j):
+#             if i==rows:
+#                 return 0
+#             sum = 1000
+#             f = j+1
+#             for k in range(j,f+1):
+#                 sum = min(rec(i+1,k)+triangle[i][j],sum) 
             
-            result = sum 
-            return result
-        if rows==1:
-            return triangle[0][0]
-        return rec(0,0)
+#             result = sum 
+#             return result
+#         if rows==1:
+#             return triangle[0][0]
+#         return rec(0,0)
 # s = Solution()
 # s.minimumTotal([-10])
+class Solution:
+     def minimumTotal(self, triangle):
+            dp = [0] *  (len(triangle) + 1)
+            for row in triangle[::-1]:
+                for i, n in enumerate(row):
+                    dp[i] = n + min (dp[i], dp[i+1])
+            return dp[0]
                 
                 
         
