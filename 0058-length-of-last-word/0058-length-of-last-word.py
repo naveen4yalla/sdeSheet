74fac258eb177d0e1eda1c4775e8bc0f39115a47
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-06-16 22:08:12 -0700

    Time: 39 ms (80.75%), Space: 16.5 MB (14.27%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,7 +1,18 @@
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         s= s.lstrip()
+#         s = s.rstrip()
+#         s = s.split()
+#         return len(s[-1])
+#alternative solution
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s= s.lstrip()
-        s = s.rstrip()
-        s = s.split()
-        return len(s[-1])
-        
+        p , length = len(s) , 0
+        while p>0:
+            p-=1
+            if s[p]!= ' ':
+                length = length + 1
+            elif length > 0:
+                return length
+        return length
+                
