bf4bb487f39c1f8cb840597b1d76928a75553553
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-06-20 14:57:05 -0700

    Time: 52 ms (18.66%), Space: 16.2 MB (92.69%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        dict1 = {}
+        s = s.split(' ')
+        
+        if len(s) != len(pattern):
+            return False
+        
+        for i in range(len(s)):
+            char_key =   'c'+pattern[i]
+            char_word =  'w'+s[i]
+            
+            
+            if char_key not in dict1:
+                dict1[char_key] = i
+                
+            if char_word not in dict1:
+                dict1[char_word] = i 
+            
+            if dict1[char_key] != dict1[char_word]:
+                return False
+            
+        return True
+        
