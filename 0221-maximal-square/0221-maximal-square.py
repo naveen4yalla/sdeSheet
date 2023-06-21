013f4ec4f31514662ce15609d1c7f6f49c812a43
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-06-21 02:33:25 -0700

    Time: 668 ms (89.08%), Space: 19.2 MB (66.54%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,48 +1,34 @@
 class Solution:
-    def maximalSquare(self, mat):
-        if not mat or not len(mat):
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix:
             return 0
-        T = [[0 for x in range(len(mat[0]))] for y in range(len(mat))]
-        maxvalue = 0
-
-        # fill in a bottom-up manner
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                T[i][j] = int(mat[i][j])
-
-                # if we are not at the first row or first column and the
-                # current cell has value 1
-                if i > 0 and j > 0 and mat[i][j] == "1":
-                    # the largest square submatrix ending at `mat[i][j]` will be 1 plus
-                    # minimum of the largest square submatrix ending at `mat[i][j-1]`,
-                    # `mat[i-1][j]` and `mat[i-1][j-1]`
-
-                    T[i][j] = min(T[i][j - 1], T[i - 1][j], T[i - 1][j - 1]) + 1
-
-                # update maximum size found so far
-                if maxvalue < T[i][j]:
-                    maxvalue = T[i][j]
-
-        # return size of the largest square matrix
-        return maxvalue ** 2
-#         directions = [(0,1),(1,0),(1,1)]
-#         m , n = len(matrix),len(matrix[0])
-#         @lru_cache(maxsize=128)
-#         def maxSquareHelper(i,j,size,maxSize):
-            
-#             if i<=m-1 and j<=n-1 and i>=0 and j>=0:
-                
-#                 left,maxSize = maxSquareHelper(i+0,j+1,size,maxSize)
-#                 down,maxSize= maxSquareHelper(i+1,j+0,size,maxSize)
-#                 diagonal,maxSize = maxSquareHelper(i+1,j+1,size,maxSize)
-#                 if matrix[i][j] == "0":
-#                     size = 0
-#                 else:
-#                     size = 1 + min(left,down,diagonal)
-#                 return size,max(size,maxSize)
-#             else:
-#                 return 0,maxSize
         
-#         size , maxSize = maxSquareHelper(0,0,0,0)
-#         return maxSize ** 2
-          
+        rows = len(matrix)
+        cols = len(matrix[0])
+        max_side = 0
+        
+        # Create an auxiliary 1D array to store the previous row's values
+        prev_row = [0] * (cols + 1)
+        
+        # Iterate over each cell in the matrix
+        for i in range(1, rows + 1):
+            # Create a variable to store the value of the previous column
+            prev_col = 0
+            
+            for j in range(1, cols + 1):
+                # Store the current cell value before updating it
+                temp = prev_row[j]
+                
+                if matrix[i - 1][j - 1] == '1':
+                    # Calculate the side length of the current square based on the surrounding cells
+                    prev_row[j] = min(prev_row[j - 1], prev_row[j], prev_col) + 1
+                    max_side = max(max_side, prev_row[j])
+                else:
+                    # If the current cell is '0', set the value in the auxiliary array to 0
+                    prev_row[j] = 0
+                
+                # Update the value of the previous column for the next iteration
+                prev_col = temp
+        
+        # Return the area of the largest square
+        return max_side * max_side
