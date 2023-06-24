ced42a4b8053c0767752ac3edf052798f8472b99
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-06-23 23:54:38 -0700

    Time: 48 ms (52.50%), Space: 16.3 MB (75.08%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,31 +1,31 @@
-# class Solution:
-#     def mergeAlternately(self, word1: str, word2: str) -> str:
-#         if len(word1) == 0: return word2
-#         if len(word2) == 0: return word1
-        
-#         m = 0
-#         n = 0
-#         result = ""
-#         while m != len(word1) and n != len(word2):
-#             result = result + word1[m] + word2[n]
-#             m+=1
-#             n+=1
-#         if m!= len(word1): return result+word1[m:]
-#         if n!= len(word2): return result+word2[n:]
-        
-#         return result
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        m,n = len(word1),len(word2)
+        i,j = 0,0
+        res = ''
+        while i<m and j<n:
+            res += word1[i]
+            res += word2[j]
+            i += 1
+            j += 1
+            
+        if i<m:
+            res += word1[i:]
+        if j<n:
+            res += word2[j:]
+        return res
         
 #one pointer technique
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        result = []
-        n = max(len(word1), len(word2))
-        for i in range(n):
-            if i < len(word1):
-                result += word1[i]
-            if i < len(word2):
-                result += word2[i]
+# class Solution(object):
+#     def mergeAlternately(self, word1, word2):
+#         result = []
+#         n = max(len(word1), len(word2))
+#         for i in range(n):
+#             if i < len(word1):
+#                 result += word1[i]
+#             if i < len(word2):
+#                 result += word2[i]
 
-        return "".join(result)
+#         return "".join(result)
         
         
