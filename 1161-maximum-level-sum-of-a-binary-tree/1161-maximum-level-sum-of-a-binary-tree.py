29ad501308246c9490061cb7195160f4e3fb5eec
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-06-25 16:38:45 -0700

    Time: 328 ms (27.78%), Space: 21.3 MB (9.91%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -4,54 +4,54 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-#         ls = []
-#         if root is None:
-#             retun 
-#         ls.append([root,1])
-#         maxSum = float("-inf")
-#         level = 0
-#         count = 0
-#         while ls:
-#             n = len(ls)
-#             s = 0
-#             count = count + 1
-#             for i in range(n):
-#                 a ,b = ls.pop(0)
-#                 if a.left:
-#                     ls.append([a.left,b+1])
-#                 if a.right:
-#                     ls.append([a.right,b+1])
-#                 s = a.val + s
-#             if s > maxSum:
-#                 maxsum = s
-#                 level = count
-#         return level
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
         ls = []
         if root is None:
-            return
-        ls.append([root, 1])
+            return 
+        ls.append([root,1])
         maxSum = float("-inf")
         level = 0
         count = 0
         while ls:
             n = len(ls)
             s = 0
-            count += 1
-            for _ in range(n):
-                a, b = ls.pop(0)
+            count = count + 1
+            for i in range(n):
+                a ,b = ls.pop(0)
                 if a.left:
-                    ls.append([a.left, b + 1])
+                    ls.append([a.left,b+1])
                 if a.right:
-                    ls.append([a.right, b + 1])
-                s += a.val
+                    ls.append([a.right,b+1])
+                s = a.val + s
             if s > maxSum:
                 maxSum = s
                 level = count
         return level
+# class Solution:
+#     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+#         ls = []
+#         if root is None:
+#             return
+#         ls.append([root, 1])
+#         maxSum = float("-inf")
+#         level = 0
+#         count = 0
+#         while ls:
+#             n = len(ls)
+#             s = 0
+#             count += 1
+#             for _ in range(n):
+#                 a, b = ls.pop(0)
+#                 if a.left:
+#                     ls.append([a.left, b + 1])
+#                 if a.right:
+#                     ls.append([a.right, b + 1])
+#                 s += a.val
+#             if s > maxSum:
+#                 maxSum = s
+#                 level = count
+#         return level
                 
                 
                 
