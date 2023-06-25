df3f2bc5d8eb78eff451ebb2ca9881dc96377d8e
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-06-24 22:19:52 -0700

    Time: 41 ms (97.44%), Space: 16.6 MB (15.82%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,11 +1,14 @@
+# class Solution:
+#     def uniqueOccurrences(self, arr: List[int]) -> bool:
+#         h1 = {}
+#         for f in arr:
+#             if f not in h1 :
+#                 h1[f] = 1
+#             else:
+#                 h1[f] += 1
+#         s1 = set(h1.values())
+#         return len(s1) == len(h1)
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        h1 = {}
-        for f in arr:
-            if f not in h1 :
-                h1[f] = 1
-            else:
-                h1[f] += 1
-        s1 = set(h1.values())
-        return len(s1) == len(h1)
+        return len(set(Counter(arr).values())) == len(set(arr))
         
