8fb1af4c35e4ff92f30302d8be2aeee1ed1fe2e4
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-06-26 18:13:05 -0700

    Time: 175 ms (20.53%), Space: 20.1 MB (23.40%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,31 +1,27 @@
-from typing import List
-
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+    def maxAreaOfIsland(self, grid):
         rows = len(grid)
         cols = len(grid[0])
         visited = set()
         result = 0
-        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
         
         def isInvalid(r, c):
             return r >= rows or r < 0 or c >= cols or c < 0
         
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        
+        def dfs(r, c):
+            if isInvalid(r, c) or (r, c) in visited or grid[r][c] == 0:
+                return 0
+            visited.add((r, c))
+            res = 0
+            for i, j in directions:
+                res = res + dfs(r + i, c + j)
+            return res+1
+        
         for i in range(rows):
             for j in range(cols):
-                if (i, j) not in visited and grid[i][j] == 1:
-                    area = 0
-                    stack = [(i, j)]
-                    while stack:
-                        r, c = stack.pop()
-                        if (r, c) not in visited and grid[r][c] == 1:
-                            visited.add((r, c))
-                            area += 1
-                            for dr, dc in directions:
-                                nr, nc = r + dr, c + dc
-                                if not isInvalid(nr, nc) and (nr, nc) not in visited and grid[nr][nc] == 1:
-                                    stack.append((nr, nc))
-                    result = max(result, area)
+                result = max(result, dfs(i, j))
         
         return result
         
