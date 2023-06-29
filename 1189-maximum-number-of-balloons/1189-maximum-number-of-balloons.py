f9197fc72d1b16c010a819f81e4c9edee1c17835
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-06-28 20:33:56 -0700

    Time: 64 ms (5.18%), Space: 16.4 MB (74.52%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,6 +1,22 @@
+# class Solution:
+#     def maxNumberOfBalloons(self, text: str) -> int:
+#         counter = collections.Counter(text)
+#         counter['l'] //= 2
+#         counter['o'] //= 2
+#         return min(counter[c] for c in 'balon')
+from collections import Counter
+
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        counter = collections.Counter(text)
-        counter['l'] //= 2
-        counter['o'] //= 2
-        return min(counter[c] for c in 'balon')
+        ls = Counter(text)
+        inputText = "balloon"
+        count = 0
+        i = 0
+        while True:
+            if i >= len(inputText):
+                i = 0
+            if inputText[i] not in ls or ls[inputText[i]] == 0:
+                return count // len(inputText)
+            ls[inputText[i]] -= 1
+            i += 1
+            count += 1
