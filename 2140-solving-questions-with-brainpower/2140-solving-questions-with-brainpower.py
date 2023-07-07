f10198496823557b686549e347b9d85eb340fe89
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-06 20:07:35 -0700

    Time: 1576 ms (96.34%), Space: 62.6 MB (56.20%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,15 +1,39 @@
-class Solution:
-    def mostPoints(self, questions: List[List[int]]) -> int:
-        dp = {}
-        n = len(questions)
+#It uses a recurive technique
+# class Solution:
+#     def mostPoints(self, questions: List[List[int]]) -> int:
+#         dp = {}
+#         n = len(questions)
         
-        def dfs(q):
-            if q >= n:
-                return 0
-            if q in dp:
-                return dp[q]
-            p, s = questions[q]
-            dp[q] = max(dfs(q + 1), p + dfs(q + s + 1))
-            return dp[q]
+#         def dfs(q):
+#             if q >= n:
+#                 return 0
+#             if q in dp:
+#                 return dp[q]
+#             p, s = questions[q]
+#             dp[q] = max(dfs(q + 1), p + dfs(q + s + 1))
+#             return dp[q]
 
-        return dfs(0)
+#         return dfs(0)
+#Iterative approach 
+class Solution:
+    def mostPoints(self, questions):
+        n = len(questions)
+        dp = [0] * n
+        dp[n - 1] = questions[n - 1][0]
+
+        for i in range(n - 2, -1, -1):
+            score = 0
+            cool = questions[i][1]
+
+            if cool + i + 1 >= n:
+                score += questions[i][0]
+            else:
+                score += questions[i][0] + dp[i + cool + 1]
+
+            dp[i] = max(score, dp[i + 1])
+
+        return dp[0]
+
+
+                
+            
