544c09cf1591d5201bf9f40cba2d6b1e14a03d75
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-21 02:43:01 -0700

    Time: 52 ms (81.36%), Space: 16.4 MB (62.44%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,20 +1,35 @@
+# class Solution:
+#     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+#         n = len(costs) // 2
+#         memo = {}
+        
+#         def dp(i, a_count, b_count):
+#             if i == 2 * n:
+#                 return 0
+#             if (i, a_count, b_count) in memo:
+#                 return memo[(i, a_count, b_count)]
+            
+#             cost_a = costs[i][0] + dp(i + 1, a_count + 1, b_count)
+#             cost_b = costs[i][1] + dp(i + 1, a_count, b_count + 1)
+            
+#             if a_count == n:
+#                 memo[(i, a_count, b_count)] = cost_b
+#             else:
+#                 memo[(i, a_count, b_count)] = min(cost_a, cost_b)
+            
+#             return memo[(i, a_count, b_count)]
+        
+#         return dp(0, 0, 0)
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        n = len(costs) // 2
-        memo = {}
-        
-        def dp(i, a_count, b_count):
-            if i == 2 * n:
-                if a_count == n and b_count == n:
-                    return 0
-                return float('inf')
-            if (i, a_count, b_count) in memo:
-                return memo[(i, a_count, b_count)]
-            
-            cost_a = costs[i][0] + dp(i + 1, a_count + 1, b_count)
-            cost_b = costs[i][1] + dp(i + 1, a_count, b_count + 1)
-            
-            memo[(i, a_count, b_count)] = min(cost_a, cost_b)
-            return memo[(i, a_count, b_count)]
-        
-        return dp(0, 0, 0)
+        diffs = []
+        for c1, c2 in costs:
+            diffs.append([c2 - c1, c1, c2])
+        diffs.sort()
+        res = 0
+        for i in range(len(diffs)):
+            if i < len(diffs) / 2:
+                res += diffs[i][2]
+            else:
+                res += diffs[i][1]
+        return res
