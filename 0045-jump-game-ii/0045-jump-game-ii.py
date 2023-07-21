9e36b50de2d73b5a9a7b4112373532056e5c5090
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-20 20:02:54 -0700

    Time: 141 ms (63.22%), Space: 17.5 MB (50.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,26 +1,37 @@
-from typing import List
+# from typing import List
 
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         if n == 1:
+#             return 0
+        
+#         queue = [(0, 0)]  # Initialize the queue with (index, jumps) tuple, starting from the first index
+#         visited = set()  # Keep track of visited nodes
+        
+#         while queue:
+#             index, jumps = queue.pop(0)  # Get the first element from the queue
+#             visited.add(index)  # Mark the current node as visited
+            
+#             for jump_length in range(nums[index], 0, -1):
+#                 next_index = index + jump_length
+                
+#                 if next_index >= n - 1:
+#                     return jumps + 1
+                
+#                 if next_index not in visited:
+#                     queue.append((next_index, jumps + 1))
+        
+#         return -1  # If the last index is not reachable, return -1
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        cache = {}  # Memoization cache
-        
-        def dp(position):
-            if position == n - 1:
-                return 0  # Base case: We have already reached the last index
-                
-            if position in cache:
-                return cache[position]  # Return the result from the cache if available
-            
-            min_jumps = float('inf')  # Initialize minimum jumps to a large value
-            
-            # Try all possible jumps from the current position
-            for jump_length in range(1, nums[position] + 1):
-                next_position = position + jump_length
-                if next_position < n:
-                    min_jumps = min(min_jumps, 1 + dp(next_position))
-            
-            cache[position] = min_jumps  # Store the result in the cache
-            return min_jumps
-        
-        return dp(0)  # St
+        l, r = 0, 0
+        res = 0
+        while r < (len(nums) - 1):
+            maxJump = 0
+            for i in range(l, r + 1):
+                maxJump = max(maxJump, i + nums[i])
+            l = r + 1
+            r = maxJump
+            res += 1
+        return res
