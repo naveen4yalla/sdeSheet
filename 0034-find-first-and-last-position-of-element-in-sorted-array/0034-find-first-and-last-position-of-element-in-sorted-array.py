dfc2027f3bede7d4e8a53a7c2b7ca25cde7cda39
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-22 01:28:01 -0700

    Time: 101 ms (45.77%), Space: 17.9 MB (30.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,59 +1,28 @@
-class Solution(object): 
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.binSearch(nums, target, True)
+        right = self.binSearch(nums, target, False)
+        return [left, right]
 
-#### Main function
-
-    def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        result = [-1,-1] #1
-        
-        result[0] = self.findStartingIndex(nums, target)  #2 
-        result[1] = self.findEndingIndex(nums, target)  #3
-        
-        return result #4
-		
-#### Helper function 1 
-
-    def findStartingIndex(self, nums, target):
-        index = -1 #5 
-        low, high = 0, len(nums) -1 #6
-        
-        while low <= high: #7
-            mid = low + (high - low)//2 #8
-			
-            if nums[mid] == target: #9
-                index = mid #10
-                high = mid - 1 #11
-            elif nums[mid] > target:  #12
-                high = mid - 1 #13
-            else:  #14
-                low = mid + 1 #15       
-        
-        return index
-        
-#### Helper function 2
-  
-    def findEndingIndex(self, nums, target):
-        index = -1
-        low, high = 0, len(nums) -1
-        
-        while low <= high:
-            
-            mid = low + (high - low)//2
-            
-            if nums[mid] == target:
-                index = mid
-                low = mid + 1 #16
-            elif nums[mid] > target: 
-                high = mid - 1
+    # leftBias=[True/False], if false, res is rightBiased
+    def binSearch(self, nums, target, leftBias):
+        l, r = 0, len(nums) - 1
+        i = -1
+        while l <= r:
+            m = (l + r) // 2
+            if target > nums[m]:
+                l = m + 1
+            elif target < nums[m]:
+                r = m - 1
             else:
-                 low = mid + 1
+                i = m
+                if leftBias:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return i
+
             
-            
-        
-        return index
+    
         
         
