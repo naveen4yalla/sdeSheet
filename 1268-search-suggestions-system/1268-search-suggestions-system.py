e39d4081af9aadf53a9132d6e955b7e03315afaa
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-22 04:11:29 -0700

    Time: 92 ms (80.90%), Space: 19.5 MB (54.87%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,64 +1,90 @@
-class TrieNode:
-    """A node in the trie structure"""
+# class TrieNode:
+#     """A node in the trie structure"""
 
-    def __init__(self, char):
-        self.char = char
-        self.is_end = False
-        self.counter = 0
-        self.children = {}
+#     def __init__(self, char):
+#         self.char = char
+#         self.is_end = False
+#         self.counter = 0
+#         self.children = {}
 
-class Trie:
-    def __init__(self):
-        self.root = TrieNode("")
+# class Trie:
+#     def __init__(self):
+#         self.root = TrieNode("")
     
-    def insert(self, word):
-        node = self.root
-        for char in word:
-            if char in node.children:
-                node = node.children[char]
-            else:
-                new_node = TrieNode(char)
-                node.children[char] = new_node
-                node = new_node
-        node.is_end = True
+#     def insert(self, word):
+#         node = self.root
+#         for char in word:
+#             if char in node.children:
+#                 node = node.children[char]
+#             else:
+#                 new_node = TrieNode(char)
+#                 node.children[char] = new_node
+#                 node = new_node
+#         node.is_end = True
 
-        node.counter += 1
+#         node.counter += 1
         
-    def dfs(self, node, prefix):
-        if node.is_end:
-            self.output.append(prefix)
+#     def dfs(self, node, prefix):
+#         if node.is_end:
+#             self.output.append(prefix)
         
-        for char in node.children:
-            self.dfs(node.children[char], prefix + char)
+#         for char in node.children:
+#             self.dfs(node.children[char], prefix + char)
     
-    def query(self, x):
-        self.output = []
-        node = self.root
+#     def query(self, x):
+#         self.output = []
+#         node = self.root
         
-        for char in x:
-            if char in node.children:
-                node = node.children[char]
-            else:
-                return []
-        self.dfs(node, x)
-        return self.output
+#         for char in x:
+#             if char in node.children:
+#                 node = node.children[char]
+#             else:
+#                 return []
+#         self.dfs(node, x)
+#         return self.output
 
 class Solution:
-    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        T = Trie()
+    def suggestedProducts(self, products, searchWord):
         result = []
-        
-        for i in products:
-            T.insert(i)
-        
-        prefix = ""
-        for j in searchWord:
-            prefix += j
-            temp = T.query(prefix)
-            temp.sort()  # Sort the suggestions lexicographically
-            if len(temp) > 3:
-                temp = temp[:3]  # Get the first three lexicographically minimum products
-            result.append(temp)
-        
+        products.sort()  # Sort the products list to use binary search
+
+        left, right = 0, len(products) - 1
+        for i in range(len(searchWord)):
+            c = searchWord[i]
+
+            while left <= right and (len(products[left]) <= i or products[left][i] != c):
+                left += 1
+
+            while left <= right and (len(products[right]) <= i or products[right][i] != c):
+                right -= 1
+
+            suggestions = []
+            remain = right - left + 1
+            for j in range(min(3, remain)):
+                suggestions.append(products[left + j])
+
+            result.append(suggestions)
+
         return result
+            
+        
+#         T = Trie()
+#         result = []
+        
+#         for i in products:
+#             T.insert(i)
+        
+#         prefix = ""
+#         for j in searchWord:
+#             prefix += j
+#             temp = T.query(prefix)
+#             temp.sort()  # Sort the suggestions lexicographically
+#             if len(temp) > 3:
+#                 temp = temp[:3]  # Get the first three lexicographically minimum products
+#             result.append(temp)
+        
+#         return result
+
+          
+
 
