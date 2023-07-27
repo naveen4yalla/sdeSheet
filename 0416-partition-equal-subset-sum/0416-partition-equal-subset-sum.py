9733a281e587b21bfc4dbbdd452eef6753e3c11b
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-26 21:52:47 -0700

    Time: 893 ms (70.34%), Space: 81.2 MB (18.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,13 +1,59 @@
+# class Solution:
+#     def canPartition(self, nums: List[int]) -> bool:
+#         total_sum = sum(nums)
+
+#         # If the total sum is odd, it's impossible to partition the array into two equal-sum subsets.
+#         if total_sum % 2 != 0:
+#             return False
+
+#         target_sum = total_sum // 2
+#         memo = {}
+
+#         def dfs(i, current_sum):
+#             if current_sum == target_sum:
+#                 return True
+
+#             if i == len(nums) or current_sum > target_sum:
+#                 return False
+
+#             if (i, current_sum) in memo:
+#                 return memo[(i, current_sum)]
+
+#             # Try including the current element in the subset.
+#             if dfs(i + 1, current_sum + nums[i]):
+#                 memo[(i, current_sum)] = True
+#                 return True
+
+#             # Try excluding the current element from the subset.
+#             if dfs(i + 1, current_sum):
+#                 memo[(i, current_sum)] = True
+#                 return True
+
+#             memo[(i, current_sum)] = False
+#             return False
+
+#         return dfs(0, 0)
+from functools import lru_cache
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
+    def canPartition(self, nums):
         @lru_cache(maxsize=None)
-        def dp(index,target):
-            if target == 0:
+        def dfs(nums, n, subset_sum):
+            # Base cases
+            if subset_sum == 0:
                 return True
-            if index==0 or target <0:
+            if n == 0 or subset_sum < 0:
                 return False
-            return dp(index-1,target-nums[index-1]) or dp(index-1,target)
-        totalSum = sum(nums)
-        if totalSum%2 != 0:
+            result = (dfs(nums, n - 1, subset_sum - nums[n])
+                    or dfs(nums, n - 1, subset_sum))
+            return result
+
+        # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
             return False
-        return dp(len(nums)-1 , totalSum//2)
+
+        subset_sum = total_sum // 2
+        n = len(nums)
+        return dfs(tuple(nums), n - 1, subset_sum)
