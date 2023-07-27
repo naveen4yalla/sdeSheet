dda605de1961465771b97cdbe0aa7b6e5901ffd2
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-26 21:28:59 -0700

    Time: 53 ms (66.15%), Space: 16.3 MB (97.58%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,24 +1,54 @@
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         memo = {}
+
+#         def dfs(i):
+#             if i == len(s):
+#                 return True
+
+#             if i in memo:
+#                 return memo[i]
+
+#             ans = False
+#             for word in wordDict:
+#                 if word == s[i:i + len(word)]:
+#                     ans = dfs(i + len(word))
+#                     if ans:
+#                         break
+
+#             memo[i] = ans
+#             return ans
+
+#         return dfs(0)
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        memo = {}
 
-        def dfs(i):
-            if i == len(s):
-                return True
+        dp = [False] * (len(s)+1)
+        dp[len(s)] = True
 
-            if i in memo:
-                return memo[i]
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
 
-            ans = False
-            for word in wordDict:
-                if word == s[i:i + len(word)]:
-                    ans = dfs(i + len(word))
-                    if ans:
-                        break
+        return dp[0]
 
-            memo[i] = ans
-            return ans
 
-        return dfs(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     
         
