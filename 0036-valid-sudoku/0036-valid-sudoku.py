ace811063a970073995001f3a7c0fbd9f6ff5e82
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-28 01:10:40 -0700

    Time: 103 ms (98.02%), Space: 16.4 MB (15.66%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,23 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        row_dict = {i:set() for i in range(9)}
-        col_dict = {i:set() for i in range (9)}
-        sub_dict = {i:set() for i in range(9)}
-        
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != '.':
-                    if board[i][j] in row_dict[i] :
-                        return False
-                    row_dict[i].add(board[i][j])
-                    if board[i][j] in col_dict[j]:
-                        return False
-                    col_dict[j].add(board[i][j])
-                    sub = 3 * (i//3) + (j//3)
-                    if board[i][j] in sub_dict[sub]:
-                        return False
-                    sub_dict[sub].add(board[i][j])
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
         return True
                     
         
