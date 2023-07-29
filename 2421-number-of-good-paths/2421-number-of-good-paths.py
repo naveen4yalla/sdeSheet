b8626c5e663627dab5ebb70c7a767cfae90a5445
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-07-29 06:20:54 -0700

    Time: 1927 ms (85.31%), Space: 34.8 MB (80.65%) - LeetHub

---
+++ 2421-number-of-good-paths/2421-number-of-good-paths.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+class UnionFind:
+    def __init__(self,n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    def find(self,i):
+        while i != self.parent[i]:
+            self.parent[i] = self.find(self.parent[i])
+            i = self.parent[i]
+        return i
+    def union(self,a,b):
+        aroot = self.find(a)
+        broot = self.find(b)
+
+        if aroot == broot:
+            return False
+        if self.rank[aroot] > self.rank[broot]:
+            self.parent[broot] = aroot
+        elif self.rank[broot] > self.rank[aroot]:
+            self.parent[aroot] = broot
+        else:
+            self.rank[aroot]+=1
+            self.parent[broot] = aroot
+        return True 
+
+        
+class Solution:
+    def numberOfGoodPaths(self, vals, edges):
+        graph = defaultdict(list)
+        for a,b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        valToIndex = defaultdict(list)
+        for i , val in enumerate(vals):
+            valToIndex[val].append(i)
+        
+        res = 0
+        uf= UnionFind(len(vals))
+        for val in sorted(valToIndex.keys()):
+            for i in valToIndex[val]:
+                for n in graph[i]:
+                    if vals[i]>= vals[n]:
+                        uf.union(n,i)
+            count = defaultdict(int)
+            for i in valToIndex[val]:
+                count[uf.find(i)] += 1
+                res += count[uf.find(i)]
+        return res
+s=Solution()
+s.numberOfGoodPaths([1,3,2,1,3],[[0,1],[0,2],[2,3],[2,4]])
