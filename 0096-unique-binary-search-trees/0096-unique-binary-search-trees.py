387bc72135d1b77668ec357018d8877ce9b7debb
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-08-05 17:09:19 -0700

    Time: 33 ms (96.55%), Space: 16.2 MB (92.09%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = {0:1, 1:1, 2:2}
+        if n < 3: return dp[n]
+        for i in range(3, n+1):
+            num = 0
+            for j in range(i):
+                num += dp[j]*dp[i-j-1]
+            dp[i] = num
+        return dp[n]
