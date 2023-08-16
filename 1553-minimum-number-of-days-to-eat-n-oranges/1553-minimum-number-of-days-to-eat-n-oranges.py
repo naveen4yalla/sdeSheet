f49d40233d5c72756bf932bd96ccd0bceeab98de
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-08-16 15:56:26 -0700

    Time: 51 ms (74.59%), Space: 17.2 MB (40.26%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minDays(self, n: int) -> int:
+        dp = {0:0,1:1}
+        
+        def dfs(n):
+            if n in dp:
+                return dp[n]
+            #we can consider two options
+            one = 1 + n%2 +dfs(n//2)
+            two = 1 + n%3 + dfs(n//3)
+            dp[n] = min(one,two)
+            
+            return dp[n]
+        return dfs(n)
+            
