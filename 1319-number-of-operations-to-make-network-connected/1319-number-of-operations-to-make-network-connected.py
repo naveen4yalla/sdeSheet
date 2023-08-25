d6e4f954675214b812818335b7634adccf264a35
Naveen Yalla <45757384+naveen4yalla@users.noreply.github.com>
2023-08-24 18:07:48 -0700

    Time: 474 ms (42.76%), Space: 36.2 MB (98.28%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,36 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_x] = root_y
+                self.rank[root_y] += 1
+
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n - 1:
+            return -1
+
+        uf = UnionFind(n)
+        components = n
+
+        for a, b in connections:
+            if uf.find(a) != uf.find(b):
+                uf.union(a, b)
+                components -= 1
+
+        return components - 1
